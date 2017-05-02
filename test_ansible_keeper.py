0c51ba5c1d226455190a8de7ca5314d0e40bec59
Jan Kogut <mr.jkogut@gmail.com>
2017-05-02 16:50:46 +0200

    TESTS: test for splitZnodeString function implemented

---
+++ test_ansible_keeper.py
@@ -0,0 +1,31 @@
+'''       
+Tests for migrate_druid_segments_hdfs2s3.py.
+'''
+
+__author__ = "Jan Kogut"
+__copyright__ = "Jan Kogut"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Jan Kogut"
+__status__ = "Beta"
+
+
+import pytest
+from ansibleKeeper import *
+
+
+
+def test_splitZnodeString():
+    '''
+    Lets test our parser
+    '''
+
+    testTup = ({"string":"groupname1:hostname1,var1:val1,var2:val2,var3:val3",
+                "output":{"groupname1":{"hostname1":{"var1":"val1", "var2":"val2", "var3":"val3"}}}},
+               {"string":"kafka:kafka01,fqdn:kafka01.fqdn.com,ipv4:1.2.3.4,kafka-id:1",
+                "output":{"kafka":{"kafka01":{"fqdn":"kafka01.fqdn.com","ipv4":"1.2.3.4","kafka-id":"1"}}}}
+                )
+
+    for varDict in testTup:
+        assert splitZnodeString(varDict['string']) == varDict['output']
+        
