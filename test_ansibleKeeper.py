0c13a2aa332897f2350c1433aa0a98acf1ebf1ab
Jan Kogut <mr.jkogut@gmail.com>
2017-05-03 19:31:41 +0200

    TEST: new tests for checking against zookeeper connection added

--- test_ansibleKeeper.py
+++ test_ansibleKeeper.py
@@ -12,12 +12,48 @@ __status__ = "Beta"
 
 import pytest
 from ansibleKeeper import *
+from kazoo.client import KazooClient
+from kazoo.handlers.threading import *
 
 
+def test_zookeeperClusterConnection():
+    '''
+    Test if we can connect to zookeeper cluster servers.
+    '''
 
+    ## example server list string 'zoo1.dmz:2181,zoo2.dmz:2181,zoo3.dmz:2181'
+    zk = KazooClient(hosts=cfg.zkServers)
+    zk.start()
+        
+    assert type(zk).__name__ == 'KazooClient'
+
+    zk.stop()
+
+
+def test_zookeeperServerConnection():
+    '''
+    Test if we can connect to each zookeeper server.
+    '''
+
+    ## example server list string 'zoo1.dmz:2181,zoo2.dmz:2181,zoo3.dmz:2181'
+
+    zkSrvList = cfg.zkServers.split(',')
+    
+    for srv in zkSrvList:
+        try:
+            zk = KazooClient(hosts=srv)
+            zk.start()
+        
+            assert type(zk).__name__ == 'KazooClient'
+        except KazooTimeoutError as error:
+            print("{0} check your connection with zookeeper server: {1} ").format(error, srv)
+
+        zk.stop()
+
+        
 def test_splitZnodeString():
     '''
-    Lets test our parser
+    Test for splitZnodeString() parser.
     '''
 
     testTup = ({"string":"groupname1:hostname1,var1:val1,var2:val2,var3:val3",
